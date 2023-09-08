52545acf7f3c7ca24ff272f513a50eafc556c37d
wy19981009 <1642837018@qq.com>
2023-09-08 16:55:52 +0800

    first commit

---
+++ src/main/java/com/wy/reggie/ReggieApplication.java
@@ -0,0 +1,20 @@
+package com.wy.reggie;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName ${NAME}
+ * @Author WY
+ * @Time 2023-9-8 16:32
+ * @Version 1.0
+ */
+@Slf4j
+@SpringBootApplication
+public class ReggieApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReggieApplication.class, args);
+        log.info("项目启动成功！");
+    }
+}
---
+++ src/main/java/com/wy/reggie/config/WebMvcConfig.java
@@ -0,0 +1,23 @@
+package com.wy.reggie.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * @ClassName WebMvcConfig
+ * @Author WY
+ * @Time 2023-9-8 16:46
+ * @Version 1.0
+ */
+@Slf4j
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        log.info("开始进行静态资源映射···");
+        registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
+        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+}
